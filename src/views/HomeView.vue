94bc362155abbba6e070bc3b263ff82549f66d32
gaebabja <hippo420@naver.com>
2024-05-28 21:40:26 +0900

    login디자인

---
+++ src/views/HomeView.vue
@@ -0,0 +1,26 @@
+<template>
+<nav>
+  <router-link to="/test">Test</router-link>
+</nav>
+
+<main>
+  <HelloWorld />
+  <RouterView />
+</main>
+
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+// Components
+import HelloWorld from '../components/HelloWorld.vue';
+
+export default defineComponent({
+  name: 'HomeView',
+
+  components: {
+    HelloWorld,
+  },
+});
+</script>
---
+++ src/views/TestView.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="about">
+    <h1>라우터 Test 페이지</h1>
+  </div>
+</template>
+<script>
+</script>
